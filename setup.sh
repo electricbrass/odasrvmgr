d7c6cbab1d9a5d81e3d134f7164068d306c8de80
Mia McMahill <miadawnmc@gmail.com>
2025-06-26 01:22:29 -0500

    Add gpl headers

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Copyright (C) 2025 Mia McMahill
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 set -euo pipefail
 
 if [[ "$EUID" -ne 0 ]]; then
--- wadfetch.py
+++ wadfetch.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2025 Mia McMahill
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import os, sys, grp, argparse, tomllib, hashlib, random, enum
 from pathlib import Path
 from typing import Iterator
